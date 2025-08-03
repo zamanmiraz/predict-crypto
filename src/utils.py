7e80bae5868079279514046f0ae8a058290c52dd
Miraz Zaman <mirazuzzaman.eee@gmail.com>
2025-08-03 03:42:45 -0400

    Add src/utils.py

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+def calculate_rsi(data, window=14):
+    delta = data.diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+def calculate_macd(df, short_window=12, long_window=26, signal_window=9):
+    df['EMA_short'] = df['weighted_price_box'].ewm(span=short_window, adjust=False).mean()
+    df['EMA_long'] = df['weighted_price_box'].ewm(span=long_window, adjust=False).mean()
+    df['MACD'] = df['EMA_short'] - df['EMA_long']
+    df['Signal_Line'] = df['MACD'].ewm(span=signal_window, adjust=False).mean()
+    return df
